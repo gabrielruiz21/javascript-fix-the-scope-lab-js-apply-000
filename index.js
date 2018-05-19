ea22a7d27b4864ce7443b0235d428889fb144ca8
GABRIEL ESTEBAN RUIZ MENESES <gabriel_ruiz21@hotmail.com>
2018-05-19 23:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,12 +21,14 @@ function add2(n) {
   return n + two
 }
 
-var funkyFunction = function() {
-  return returning()
-}
 function returning() {
     return "FUNKY!"
 }
+
+var funkyFunction = function() {
+  return returning()
+}
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
