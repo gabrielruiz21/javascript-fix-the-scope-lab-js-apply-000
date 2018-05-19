e43b013e12c1ed7ec33a3c84d5d6c81f620ff130
GABRIEL ESTEBAN RUIZ MENESES <gabriel_ruiz21@hotmail.com>
2018-05-19 23:59:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+  return returning()
+}
+function returning() {
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
